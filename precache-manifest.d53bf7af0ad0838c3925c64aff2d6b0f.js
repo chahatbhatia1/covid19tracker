91bf638592906c68de86ea5febd20d173fa646fd
Chahat Bhatia <chahatbhatia90@gmail.com>
2020-03-31 14:17:58 +0530

    Updates

---
+++ precache-manifest.d53bf7af0ad0838c3925c64aff2d6b0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad977c03d56c96f48436d6b9ac75a2a",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "4f739a43e68eb92ca6c4",
+    "url": "/covid19tracker/static/css/main.8d95b234.chunk.css"
+  },
+  {
+    "revision": "4e7ada795bb4f1b08aab",
+    "url": "/covid19tracker/static/js/2.76fa8897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19tracker/static/js/2.76fa8897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f739a43e68eb92ca6c4",
+    "url": "/covid19tracker/static/js/main.f25e62c3.chunk.js"
+  },
+  {
+    "revision": "2234d72beb6a61dc5615",
+    "url": "/covid19tracker/static/js/runtime-main.d3bd1e3b.js"
+  },
+  {
+    "revision": "2b4471cd489804736605cc69a88c13a7",
+    "url": "/covid19tracker/static/media/coronavirus.2b4471cd.jpg"
+  }
+]);
